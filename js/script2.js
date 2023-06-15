c6f3dc5d1a7f3e4ac4259ddb7e96a8bda01e541a
aswati94 <aswatistudy@gmail.com>
2023-06-15 17:55:05 +0530

    variable global declaration & block scope

---
+++ js/script2.js
@@ -0,0 +1,15 @@
+//var global declaration using block {means curly braces} scope 
+var x=10; // here var is a global variable as declared outside the block
+{ // block scope
+    console.log(x);   
+}
+{ // i can have as many as block scope lets declare let variable inside block scope & execute outside
+    let b= 30;
+}
+ // execute outside the block scope
+    console.log(b);      // through error as we can't access the variable declared inside the block scope using let keyword
+
+{
+    var b= 30;   //declare var keyword inside the block scope & execute outside 
+}
+    console.log(b);
